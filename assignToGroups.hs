b91c857ed553fb8304ffe10f09b142b77289d79b
yves-weissenberger <yvesweissenberger@gmail.com>
2022-04-05 18:02:15 +0100

    change

--- assignToGroups.hs
+++ assignToGroups.hs
@@ -5,6 +5,6 @@ import System.Directory
 
 main = do
     files <- getDirectoryContents "."
-    let groups = cycle [1,2,3]
+    let groups = cycle [1 .. 5]
     let dispatch = zip files groups
     print dispatch
